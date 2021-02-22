b648488dc778c76d94848ca01d54d7890631759d
emzys <emzysk@gmail.com>
2021-02-22 23:49:58 +0900

    update script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+const price = document.querySelector('#price');
+const people = document.querySelector('#people');
+const tip = document.querySelector('#tip');
+const btn = document.querySelector('.count');
+const errorMsg = document.querySelector('.error');
+const costInfo = document.querySelector('.cost-info');
+
+
+const isFilledOut = () => {
+
+  let finalCost;
+  if (price.value == "" || people.value == "" || tip.value == 0) {
+    errorMsg.innerText = "Please provide missing information.";
+  } else {
+    errorMsg.innerText = "";
+    costInfo.innerText = "";
+    countCost();
+  }
+};
+
+const countCost = () => {
+  const newPrice = parseFloat(price.value);
+  const newTip = parseFloat(tip.value);
+  const peopleCount = parseFloat(people.value);
+  
+  const finalCost = (newPrice + (newPrice * newTip)) / peopleCount;
+  costInfo.innerText = `The cost per person is ${finalCost.toFixed(2)}!`
+};
+
+
+btn.addEventListener('click', isFilledOut);
